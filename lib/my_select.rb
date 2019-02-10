5ba63c33d449399fa50521c9224b11b7b3e54076
Tracy Ngo <tngo1206@gmail.com>
2019-02-10 22:03:12 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,11 @@
 def my_select(collection)
- # your code here!
+ i = 0
+ updated_array = []
+ while i < collection.length
+   if (yield(collection[i]) == true)
+     updated_array << (collection[i])
+  end
+   i += 1
+ end
+  updated_array
 end
